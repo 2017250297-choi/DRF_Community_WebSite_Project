b7599fb223f880cdefdfb0b3323b1eaba59a5876
2017250297-choi <97ckdtn@gmail.com>
2023-05-14 17:01:33 +0900

    Fix : Fix Debug Option and AllowHosts

--- DRF_Community_WebSite_Project/settings.py
+++ DRF_Community_WebSite_Project/settings.py
@@ -25,9 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "3.34.125.84",
+]
 
 
 # Application definition
