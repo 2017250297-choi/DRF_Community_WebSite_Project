90a25e7630032787990fa5a2caa28a48b3a3209e
2017250297-choi <97ckdtn@gmail.com>
2023-05-14 16:46:58 +0900

    fix : Update Access Token Exp
    만료기간 단축(2분)

--- DRF_Community_WebSite_Project/settings.py
+++ DRF_Community_WebSite_Project/settings.py
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "user.User"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
